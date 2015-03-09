3eaa198e90b046d7f16e9dfc9ca49d788cfdf4b6
Garyfimo <garyfimo@gmail.com>
2015-03-09 13:01:35 -0500

    servicios and productos done

--- odontocarpio/citas/views.py
+++ odontocarpio/citas/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
-from .models import Producto
+from .models import Producto, Servicio
 # Create your views here.
 def home(request):
 	lista_productos = Producto.objects.all()
+	lista_servicios = Servicio.objects.all()
 	context = {
 		'productos' : lista_productos,
+		'servicios' : lista_servicios,
 	}
 	return render(request,'odontocarpio/index.html',context)
