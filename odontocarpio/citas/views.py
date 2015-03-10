674310badb141501699b1ef5ae33c38603a2eb17
Garyfimo <garyfimo@gmail.com>
2015-03-09 19:55:39 -0500

    more changes

--- odontocarpio/citas/views.py
+++ odontocarpio/citas/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-from .models import Producto, Servicio
+from .models import Producto, Servicio, Doctor, Banner
 # Create your views here.
 def home(request):
 	lista_productos = Producto.objects.all()
 	lista_servicios = Servicio.objects.all()
+	lista_doctores = Doctor.objects.all()
+	lista_banners = Banner.objects.all()
 	context = {
 		'productos' : lista_productos,
 		'servicios' : lista_servicios,
+		'doctores' : lista_doctores,
+		'banners' : lista_banners,
 	}
 	return render(request,'odontocarpio/index.html',context)
